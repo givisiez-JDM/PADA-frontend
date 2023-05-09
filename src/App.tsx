073b5f90c533bf109344f244082bf8a1d4a37b78
danilosmod <danilo.oldschool@gmail.com>
2023-05-09 10:42:56 -0300

    Creação projeto base vite

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <p>PADA-Frontend</p>
+      </div>
+    </>
+  )
+}
+
+export default App
