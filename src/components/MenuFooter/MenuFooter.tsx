34a3a3b4aec5400016a3938b5712e7512aefba80
danilosmod <danilo.oldschool@gmail.com>
2023-06-19 21:16:55 -0300

    Funções dos botões de rodapé

--- src/components/MenuFooter/MenuFooter.tsx
+++ src/components/MenuFooter/MenuFooter.tsx
@@ -8,11 +8,14 @@ const MenuFooter = () => {
         src="./src/assets/HomeIcone.png"
         value="Inicio"
         color="blue"
+        route="/"
       />
+
       <MenuFooterButton
         src="./src/assets/SairIcone.png"
         value="Sair"
         color="grey"
+        route="/tipo-login"
       />
     </FooterContainer>
   );
--- src/components/MenuFooterButton/MenuFooterButton.tsx
+++ src/components/MenuFooterButton/MenuFooterButton.tsx
@@ -4,17 +4,26 @@ import {
   ButtonImg,
   ButtonText,
 } from "./MenuFooterButton.styles";
+import { Link } from "react-router-dom";
 
 export type FooterButtonType = {
   src?: string;
   value?: string;
   color?: "blue" | "grey";
+  route?: string;
 };
 
-export const MenuFooterButton = ({ src, value, color }: FooterButtonType) => {
+export const MenuFooterButton = ({
+  src,
+  value,
+  color,
+  route,
+}: FooterButtonType) => {
   return (
     <ButtonContainer color={color}>
-      <ButtonImg src={src} />
+      <Link to={`${route}`}>
+        <ButtonImg src={src} />
+      </Link>
       <ButtonText>{value}</ButtonText>
     </ButtonContainer>
   );
