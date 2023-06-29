dd8a9fa84f92c5e2e8a4ed4779e42e75a6942252
danilosmod <danilo.oldschool@gmail.com>
2023-06-28 21:01:01 -0300

    mudanças menu médico

---
+++ src/pages/CadastroPaciente.tsx
@@ -0,0 +1,13 @@
+import CadastroPacienteHeader from "../components/MenuMedicoHeader/MenuMedicoHeader";
+import ConteudoCadastroPaciente from "../components/ConteudoCadastroPaciente/ConteudoCadastroPaciente";
+
+const CadastroPaciente = () => {
+  return (
+    <>
+      <CadastroPacienteHeader />
+      <ConteudoCadastroPaciente />
+    </>
+  );
+};
+
+export default CadastroPaciente;
