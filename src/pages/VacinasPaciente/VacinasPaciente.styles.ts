776ad64ac3a244ef553714b9c95e05eb7d560be6
Arthur Phillip Silva <artphil.dev@gmail.com>
2024-01-15 16:50:44 -0300

    faet: criando lista de vacinas

---
+++ src/pages/VacinasPaciente/VacinasPaciente.styles.ts
@@ -0,0 +1,27 @@
+import styled from "styled-components"
+
+export const VaccinesContainer = styled.section`
+    display: flex;
+    flex-direction: column;
+    padding: 3rem;
+    width: 100%;
+    height: 100%;
+`
+
+export const VaccinesHeader = styled.div`
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    padding: 1rem;
+`
+
+export const VaccinesTitle = styled.h1`
+    font-size: 2rem;
+`
+
+export const VaccinesList = styled.ul`
+    padding-top: 2rem;
+`
+
+export const VaccineItem = styled.li`
+`
--- src/pages/VacinasPaciente/index.tsx
+++ src/pages/VacinasPaciente/index.tsx
@@ -1,5 +1,6 @@
 
 import DefaultPatientPage from '../../components/DefaultPatientPage';
+import { VaccineItem, VaccinesContainer, VaccinesHeader, VaccinesList, VaccinesTitle, VacinasContainer, VacinasHeader, VacinasTitulo } from './VacinasPaciente.styles';
 
 const VacinasPaciente = () => {
 
@@ -9,12 +10,33 @@ const VacinasPaciente = () => {
         photo: '/vite.svg'
     }
 
-    return ( 
-        <DefaultPatientPage patient={patient}>
-            <div>Vacinas paciente</div>
+    const vaccines =[
+    {id: 1, date: '1/1/24', title: 'Vacina 1', description: 'lorem ipsum'},
+    {id: 2, date: '1/1/24', title: 'Vacina 2', description: 'lorem ipsum'},
+    {id: 3, date: '1/1/24', title: 'Vacina 3', description: 'lorem ipsum'},
+    {id: 4, date: '1/1/24', title: 'Vacina 4', description: 'lorem ipsum'},
+    {id: 5, date: '1/1/24', title: 'Vacina 5', description: 'lorem ipsum'},
+    ]
 
-        </DefaultPatientPage>        
-     );
+    return (
+        <DefaultPatientPage patient={patient}>
+            <VaccinesContainer>
+                <VaccinesHeader>
+                    <VaccinesTitle>Vacinas</VaccinesTitle>
+
+                </VaccinesHeader>
+            <VaccinesList>
+                {vaccines.map(vaccine => (
+                <VaccineItem key={vaccine.id}>
+                    {vaccine.title}
+                    </VaccineItem>
+                    ))}
+            </VaccinesList>
+
+            </VaccinesContainer>
+
+        </DefaultPatientPage>
+    );
 }
- 
+
 export default VacinasPaciente;
