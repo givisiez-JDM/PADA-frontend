26b6bf8c958f7da5ee642fad9965d223d182e1c9
Arthur Phillip Silva <artphil.dev@gmail.com>
2024-01-15 16:11:07 -0300

    feat: criando uma pagina padrão para dados do paciente

---
+++ src/components/DefaultPatientPage/index.tsx
@@ -0,0 +1,53 @@
+import { useNavigate } from "react-router-dom";
+
+import patientIcone from "../../assets/user.svg";
+import closeIcone from "../../assets/close.svg";
+import arrowRight from "../../assets/arrow-right.svg";
+
+import MenuHeader from '../MenuHeader/MenuHeader';
+
+import { Close, Content, Main, Menu, MenuItem, PatientData, Picture, SideBar } from './DefaultPatientPage.styles';
+
+interface IUser {
+    name: string,
+}
+
+interface IPatient {
+    name: string,
+    photo: any
+}
+
+interface Props
+    extends React.ComponentProps<"main"> {
+    user?: IUser,
+    patient?: IPatient
+}
+
+const DefaultPatientPage = ({ user, patient, ...props }: Props) => {
+
+    const navigate = useNavigate();
+
+    return (
+        <Main>
+            <MenuHeader>
+                <PatientData>
+                    <Picture src={patient ? patient.photo : patientIcone} alt='foto do paciente' />
+                    <p>{patient ? patient.name : 'Nome do paciente'}</p>
+                </PatientData>
+                <Close onClick={()=> navigate('/menu-medico')}> <img src={closeIcone}  /></Close>
+            </MenuHeader>
+            <Content>
+            <SideBar>
+                <Menu>
+                    <MenuItem onClick={() => navigate('/')}><p>Perfil</p> <img src={arrowRight}/></MenuItem>
+                    <MenuItem onClick={() => navigate('/')}><p>Fases</p> <img src={arrowRight}/></MenuItem>
+                    <MenuItem onClick={() => navigate('/')}><p>Vacinas</p> <img src={arrowRight}/></MenuItem>
+                </Menu>
+            </SideBar>
+            {props.children}
+            </Content>
+        </Main>
+    );
+}
+
+export default DefaultPatientPage;
