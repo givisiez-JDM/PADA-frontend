41b696610b455adcd97a0e6b80fda4196a498221
Arthur Phillip Silva <artphil.dev@gmail.com>
2024-01-15 16:30:54 -0300

    feat: atribuindo valores e passando por props

--- src/pages/VacinasPaciente/index.tsx
+++ src/pages/VacinasPaciente/index.tsx
@@ -3,8 +3,14 @@ import DefaultPatientPage from '../../components/DefaultPatientPage';
 
 const VacinasPaciente = () => {
 
+    const patient = {
+        id: '5f59f128-148d-469b-9452-4613e6afd67c',
+        name: 'Beatrix',
+        photo: '/vite.svg'
+    }
+
     return ( 
-        <DefaultPatientPage>
+        <DefaultPatientPage patient={patient}>
             <div>Vacinas paciente</div>
 
         </DefaultPatientPage>        
