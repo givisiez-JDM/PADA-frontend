8e27d777302ed1b496c3a6ab84aad5b8b7f06be5
Arthur Phillip Silva <artphil.dev@gmail.com>
2024-01-15 16:11:07 -0300

    feat: criando uma pagina padrão para dados do paciente

---
+++ src/components/DefaultPatientPage/DefaultPatientPage.styles.ts
@@ -0,0 +1,50 @@
+import styled from "styled-components"
+
+
+export const Main = styled.main`
+    display: flex;
+    flex-direction: column;
+    font-family: 'Poppins', sans-serif;
+    height: 100vh;
+`
+
+export const PatientData = styled.div`
+    display: flex;
+    align-items: center;
+`
+export const Picture = styled.img`
+    display: flex;
+    height: 6rem;
+    margin: 1rem;
+    box-shadow: 0 4 ;
+`
+export const Close = styled.a`
+    width: 1,12rem;
+    margin: auto 0;
+
+    img {
+        width: 100%;
+    }
+`
+
+export const Content = styled.section`
+    display: flex;
+    height: 100%;
+`
+
+export const SideBar = styled.div`
+    width: 15%;
+    min-width: 130px;
+    height: 100%;
+    border-right: 2px solid #B4B4B4;
+`
+export const Menu = styled.ul`
+    padding: 5rem 2rem;
+`
+
+export const MenuItem = styled.li`
+    display: flex;
+    justify-content: space-between;
+    padding: 1rem; 
+    font-weight: 700; 
+`
