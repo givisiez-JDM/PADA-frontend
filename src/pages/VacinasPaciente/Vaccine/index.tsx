a4b5421fdaa33b25cc7013884b754a223d257f61
Arthur Phillip Silva <artphil.dev@gmail.com>
2024-01-17 16:56:12 -0300

    feat: Implementado busca por data

--- src/pages/VacinasPaciente/Vaccine/index.tsx
+++ src/pages/VacinasPaciente/Vaccine/index.tsx
@@ -2,34 +2,41 @@ import { useState } from "react";
 
 import arrowUp from '../../../assets/arrow-up.svg';
 import arrowDown from '../../../assets/arrow-down.svg';
+import { VaccineType } from "../../../Types/VaccineTypes";
 
 import { VaccineButton, VaccineContainer, VaccineDate, VaccineDescription, VaccineText, VaccineTitle } from "./Vaccine.style";
 
-interface Props {
-    id: number,
-    date: string,
-    title: string,
-    description: string,
-    status: string,
-}
-
-function Vaccine({ title, description, date, status }: Props) {
+function Vaccine({ title, observation, scheduledDate, status, applicationDate }: VaccineType) {
     const [showDescription, setShowDescription] = useState(false);
 
     function toggledescription() {
         setShowDescription(!showDescription)
     }
 
+    function getVaccineStatus() {
+        switch (status) {
+            case 'applied':
+                return 'applied'
+            case 'not applied':
+                return 'not-applied'
+            case 'late':
+                return 'late'
+            case 'schedule':
+                return 'schedule'
+            default:
+                return ''
+        }
+    }
 
     return (
 
         <VaccineContainer>
-            <VaccineDate className={status}>{date}</VaccineDate>
+            <VaccineDate className={getVaccineStatus()}>{applicationDate ? applicationDate : scheduledDate}</VaccineDate>
             <VaccineDescription>
                 <VaccineTitle>{title}</VaccineTitle>
                 <VaccineText
                     className={showDescription ? 'active' : ''}
-                >{description}</VaccineText>
+                >{observation}</VaccineText>
             </VaccineDescription>
             <VaccineButton
                 onClick={toggledescription}
