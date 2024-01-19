07f92591eddb80a5e9da92ec842affb07eadd351
artphil <artphil.dev@gmail.com>
2024-01-19 15:08:54 -0300

    refactor: renomenando imports e  corrigindo uso das props

--- src/components/defaultPatientPage/DefaultPatientPage.tsx
+++ src/components/defaultPatientPage/DefaultPatientPage.tsx
@@ -1,8 +1,7 @@
 import { useNavigate } from "react-router-dom";
 
-import patientIcone from "../../assets/user.svg";
-import closeIcone from "../../assets/close.svg";
-import arrowRight from "../../assets/arrow-right.svg";
+import CloseIco from "../../assets/close.svg";
+import ArrowRightIco from "../../assets/arrow-right.svg";
 
 import MenuHeader from '../menuHeader/MenuHeader';
 
@@ -10,15 +9,8 @@ import { PatientType } from "../../types/PatientTypes";
 
 import { Close, Content, Main, Menu, MenuItem, PatientData, Picture, SideBar } from './DefaultPatientPage.styles';
 
-///////////// TODO criar UserType
-interface IUser {
-    name: string,
-}
-///////////////////////////////
-
 interface Props
     extends React.ComponentProps<"main"> {
-    user?: IUser,
     patient: PatientType
 }
 
@@ -30,17 +22,17 @@ const DefaultPatientPage = ({ patient, ...props }: Props) => {
         <Main>
             <MenuHeader>
                 <PatientData>
-                    <Picture src={patient ? patient.photo : patientIcone} alt='foto do paciente' />
-                    <p>{patient ? patient.name : 'Nome do paciente'}</p>
+                    <Picture src={patient.photo} alt='foto do paciente' />
+                    <p>{patient.name}</p>
                 </PatientData>
-                <Close onClick={() => navigate('/menu-medico')}> <img src={closeIcone} /></Close>
+                <Close onClick={() => navigate('/menu-medico')}> <img src={CloseIco} /></Close>
             </MenuHeader>
             <Content>
                 <SideBar>
                     <Menu>
-                        <MenuItem onClick={() => navigate(`/perfil/paciente/${patient.id}`)}><p>Perfil</p> <img src={arrowRight} /></MenuItem>
-                        <MenuItem onClick={() => navigate('#')}><p>Fases</p> <img src={arrowRight} /></MenuItem>
-                        <MenuItem onClick={() => navigate(`/vacinas/paciente/${patient.id}`)}><p>Vacinas</p> <img src={arrowRight} /></MenuItem>
+                        <MenuItem onClick={() => navigate(`/perfil/paciente/${patient.id}`)}><p>Perfil</p> <img src={ArrowRightIco} /></MenuItem>
+                        <MenuItem onClick={() => navigate('#')}><p>Fases</p> <img src={ArrowRightIco} /></MenuItem>
+                        <MenuItem onClick={() => navigate(`/vacinas/paciente/${patient.id}`)}><p>Vacinas</p> <img src={ArrowRightIco} /></MenuItem>
                     </Menu>
                 </SideBar>
                 {props.children}
