ee11809a2e151f83c6cf5f2964c6f8d9847d0012
artphil <artphil.dev@gmail.com>
2024-02-05 14:44:13 -0300

    corrigindo request de phase

--- src/pages/vacinasPaciente/VacinasPaciente.tsx
+++ src/pages/vacinasPaciente/VacinasPaciente.tsx
@@ -17,27 +17,17 @@ import { VaccineColor, VaccineColorContainer, VaccinesContainer, VaccinesDate, V
 
 //////////////// TODO remover mocks e integrar com Back End
 const vaccines: VaccineType[] = [
-    { id: 1, scheduledDate: '2024-01-01', applicationDate: '2024-01-01', title: 'Vacina 1', observation: 'lorem ipsum', status: 'applied' },
-    { id: 2, scheduledDate: '2024-01-02', applicationDate: '', title: 'Vacina 2', observation: 'lorem ipsum', status: 'not applied' },
-    { id: 3, scheduledDate: '2024-01-03', applicationDate: '2024-01-04', title: 'Vacina 3', observation: 'lorem ipsum', status: 'late' },
-    { id: 4, scheduledDate: '2024-01-04', applicationDate: '', title: 'Vacina 4', observation: 'lorem ipsum', status: 'applied' },
-    { id: 5, scheduledDate: '2024-01-05', applicationDate: '', title: 'Vacina 5', observation: 'lorem ipsum', status: 'schedule' },
-    { id: 6, scheduledDate: '2024-01-06', applicationDate: '', title: 'Vacina 6', observation: 'lorem ipsum', status: 'schedule' },
-    { id: 7, scheduledDate: '2024-01-07', applicationDate: '', title: 'Vacina 7', observation: 'lorem ipsum', status: 'schedule' },
-    { id: 8, scheduledDate: '2024-01-08', applicationDate: '', title: 'Vacina 8', observation: 'lorem ipsum', status: 'schedule' },
-    { id: 9, scheduledDate: '2024-01-09', applicationDate: '', title: 'Vacina 9', observation: 'lorem ipsum', status: 'schedule' },
-    { id: 10, scheduledDate: '2024-01-10', applicationDate: '', title: 'Vacina 10', observation: 'lorem ipsum', status: 'schedule' },
+    { id: 1, scheduledDate: '2024-01-01 10:30:00', applicationDate: '2024-01-01', title: 'Vacina 1', observation: 'lorem ipsum', status: 'applied' },
+    { id: 2, scheduledDate: '2024-01-02 10:30:00', applicationDate: '', title: 'Vacina 2', observation: 'lorem ipsum', status: 'not applied' },
+    { id: 3, scheduledDate: '2024-01-03 10:30:00', applicationDate: '2024-01-04', title: 'Vacina 3', observation: 'lorem ipsum', status: 'late' },
+    { id: 4, scheduledDate: '2024-01-04 10:30:00', applicationDate: '', title: 'Vacina 4', observation: 'lorem ipsum', status: 'applied' },
+    { id: 5, scheduledDate: '2024-01-05 10:30:00', applicationDate: '', title: 'Vacina 5', observation: 'lorem ipsum', status: 'schedule' },
+    { id: 6, scheduledDate: '2024-01-06 10:30:00', applicationDate: '', title: 'Vacina 6', observation: 'lorem ipsum', status: 'schedule' },
+    { id: 7, scheduledDate: '2024-01-07 10:30:00', applicationDate: '', title: 'Vacina 7', observation: 'lorem ipsum', status: 'schedule' },
+    { id: 8, scheduledDate: '2024-01-08 10:30:00', applicationDate: '', title: 'Vacina 8', observation: 'lorem ipsum', status: 'schedule' },
+    { id: 9, scheduledDate: '2024-01-09 10:30:00', applicationDate: '', title: 'Vacina 9', observation: 'lorem ipsum', status: 'schedule' },
+    { id: 10, scheduledDate: '2024-01-10 10:30:00', applicationDate: '', title: 'Vacina 10', observation: 'lorem ipsum', status: 'schedule' },
 ]
-
-// const patient: PatientType = {
-//     id: '5f59f128-148d-469b-9452-4613e6afd67c',
-//     name: 'Beatrix',
-//     photo: '/vite.svg',
-//     email: 'Beatrix@Beatrix',
-//     telephone: '123546',
-//     birthDate: '2000-05-23',
-// }
-//////////////////////////////
 
 const VacinasPaciente = () => {
     const userRequest = new UserRequest();
@@ -67,7 +57,7 @@ const VacinasPaciente = () => {
 
     useEffect(() => {
         try {
-            const { url, headers } = userRequest.GET_PHASES_BY_TREATMENTS_ID(String(treatmant.data.id), token);
+            const { url, headers } = userRequest.GET_PHASES_BY_TREATMENTS_ID(String(treatmant.data?.id), token);
             phase.get(url, { headers });
         }
         catch (error) {
@@ -77,8 +67,8 @@ const VacinasPaciente = () => {
 
     useEffect(() => {
         try {
-            const lastItem = phase.data?.slice(- 1);
-            const { url, headers } = userRequest.GET_VACCINES_BY_PHASES_ID(String(phase.data[lastItem].id), token);
+            const lastItem = phase.data?.slice(- 1)[0];
+            const { url, headers } = userRequest.GET_VACCINES_BY_PHASES_ID(String(lastItem?.id), token);
             phase.get(url, { headers });
         }
         catch (error) {
