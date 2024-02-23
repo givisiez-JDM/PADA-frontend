8fe7e26a5cee140a802784d80e4dec3424fbb5f2
artphil <artphil.dev@gmail.com>
2024-02-23 16:27:59 -0300

    refactor: atualização do tipo Fase

--- src/types/TreatmentTypes.ts
+++ src/types/TreatmentTypes.ts
@@ -1,3 +1,7 @@
+export type AllergieType = ''
+export type DosageType = '1:10' | '1:100' | '1:1.000'| '1:10.000'
+export type FrequencyType =  '7 dias' | '2 semanas' | '3 semanas' | '4 semanas'
+
 export type TreatmentType = {
     id: number,
     allergies: string[],
@@ -9,8 +13,8 @@ export type TreatmentType = {
 export type PhaseType = {
     id: number,
     phaseNumber: number,
-    dosage: string,
-    frequency: string,
+    dosage: DosageType,
+    frequency:  FrequencyType,
     startTreatment: string,
     endTreatment: string,
     active: boolean,
