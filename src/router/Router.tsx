6af25f25fea080e42722c7d7634ce8c509351e0d
Arthur Phillip D. Silva <artphil.dev@gmail.com>
2024-02-26 14:57:05 -0300

    chore: Update Router.tsx - aspas

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/home/HomePage";
 import Cadastro from "../pages/cadastro/CadastroPage";
 import Login from "../pages/login/LoginPage";
-import GlobalStorage from '../global/UserContext'
+import GlobalStorage from "../global/UserContext";
 import MenuMedico from "../pages/menuMedico/MenuMedicoPage";
 import PerfilPaciente from "../pages/perfilPaciente/PerfilPacientePage";
 import FaseDeTratamento from "../pages/faseDeTratamento";
