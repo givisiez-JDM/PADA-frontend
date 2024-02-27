0786757f7ada8563bc615c391196e27f27933fd7
artphil <artphil.dev@gmail.com>
2024-02-27 16:29:00 -0300

    style: atualizando header de paciente

--- src/components/patientHeader/PatientHeader.tsx
+++ src/components/patientHeader/PatientHeader.tsx
@@ -1,8 +1,7 @@
 import IconPatient from "../../assets/iconPatient.svg"
 import IconClose from "../../assets/close.svg"
-import MenuHeader from "../menuHeader/MenuHeader";
 import { PatientType } from "../../types/PatientTypes";
-import { Close, IconX, Logo, PatientData } from "./PatientHeader.styles"
+import { Close, Header, IconX, Logo, PatientData } from "./PatientHeader.styles"
 
 interface HeaderProps {
   patient: PatientType | null
@@ -11,7 +10,7 @@ interface HeaderProps {
 const PatientHeader: React.FC<HeaderProps> = ({ patient }) => {
   
   return (
-    <MenuHeader>
+    <Header>
       <PatientData>
         <Logo src={patient?.photo || IconPatient} />
         <p>{patient?.name}</p>
@@ -19,7 +18,7 @@ const PatientHeader: React.FC<HeaderProps> = ({ patient }) => {
       <Close  to={'/menu-medico'}>
         <IconX src={IconClose} alt="Fechar" />
       </Close>
-    </MenuHeader>
+    </Header>
   )
 }
 
