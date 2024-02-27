f1c1ced8ca5af60104a38dc9d3f4e3a2c3f09d82
artphil <artphil.dev@gmail.com>
2024-02-27 16:57:34 -0300

    chore: apagando elementos não utilizados

--- src/components/defaultPatientPage/DefaultPatientPage.tsx
+++ src/components/defaultPatientPage/DefaultPatientPage.tsx
@@ -1,16 +1,7 @@
-import { useNavigate } from "react-router-dom";
-
-import iconClose from "../../assets/close.svg";
-import iconArrowRight from "../../assets/arrow-right.svg";
-import iconPatient from '../../assets/user.svg'
-
-import MenuHeader from '../menuHeader/MenuHeader';
-
-import { PatientType } from "../../types/PatientTypes";
-
-import { Close, Content, Main, Menu, MenuItem, PatientData, Picture, SideBar } from './DefaultPatientPage.styles';
 import PatientHeader from "../patientHeader/PatientHeader";
 import SidebarPatientPage from "../sidebarPatientPage/SidebarPatientPage";
+import { PatientType } from "../../types/PatientTypes";
+import { Content, Main } from './DefaultPatientPage.styles';
 
 interface Props
     extends React.ComponentProps<"main"> {
