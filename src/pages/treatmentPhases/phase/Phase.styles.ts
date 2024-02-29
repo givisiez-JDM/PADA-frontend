6003bb9433d0d5f801dd0e06cdd7adc6f5e39a61
artphil <artphil.dev@gmail.com>
2024-02-29 19:50:26 -0300

    chore: removendo código não utilizado

--- src/pages/treatmentPhases/phase/Phase.styles.ts
+++ src/pages/treatmentPhases/phase/Phase.styles.ts
@@ -1,19 +1,5 @@
 import styled from "styled-components";
 
-export const PhaseTitle = styled.h2`
-  font-size: 1.4rem;
-  font-weight: 600;
-  padding-bottom: 1rem;
-
-  &::before {
-    content: ".";
-    color: #4c7abb;
-    border-radius: 1rem;
-    margin-right: 0.9rem;
-    background-color: #4c7abb;
-  }
-`;
-
 export const PhaseField = styled.div`
   padding: 0.5rem 0;
 `;
