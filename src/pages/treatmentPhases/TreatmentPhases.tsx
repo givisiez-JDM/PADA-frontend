7ab2b17a56e24da87d5e9dfaf22ffe4430f3fee2
artphil <artphil.dev@gmail.com>
2024-03-04 17:39:37 -0300

    feat: integração com backend

--- src/pages/treatmentPhases/TreatmentPhases.tsx
+++ src/pages/treatmentPhases/TreatmentPhases.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import IconArrowUp from '../../assets/arrow-up.svg';
 import IconArrowDown from '../../assets/arrow-down.svg';
 import DefaultPatientPage from "../../components/defaultPatientPage/DefaultPatientPage";
@@ -8,52 +9,61 @@ import Button from "../../components/button/Button";
 import { PatientType } from "../../types/PatientTypes";
 import { PhaseType, TreatmentType } from "../../types/TreatmentTypes";
 import { BoxButton, Main, PhaseBlock, PhaseTitle, Section, Title } from "./TreatmentPhases.styles";
+import useAxios from "../../hooks/useAxios";
+import { UserRequest } from "../../requests/UserRequest";
 
-const patient: PatientType = { name: 'Ana', photo: '', id: '62d08a04-5eb6-41b1-92fc-52f6c705dc1c', birthDate: '', email: '', telephone: '' }
-const traetment: TreatmentType = {
-  id: "5b184e74-8284-487c-a48b-3c00ca3146e6",
-  allergies: [
-    "comida",
-    "ácaro"
-  ],
-  method: "vacina",
-  active: true
-}
-const phases: PhaseType[] = [
-  {
-    id: "d8ed2f0e-18ef-408f-aaa4-78b91f975902",
-    phaseNumber: 1,
-    dosage: "1:10",
-    frequency: "2 semanas",
-    startTreatment: "2024-03-02",
-    endTreatment: "2024-03-30",
-    active: false
-  },
-  {
-    id: "673abed4-ee3a-4566-84e6-18bb8d67199d",
-    phaseNumber: 2,
-    dosage: "1:100",
-    frequency: "7 dias",
-    startTreatment: "2024-05-01",
-    endTreatment: "2024-06-15",
-    active: true
-  }
-]
 
 const TreatmentPhases = () => {
+  const { id: patientId } = useParams();
+  const token = window.localStorage.getItem("token");
+
+  const userRequest = new UserRequest();
+  const patientReq = useAxios<PatientType>();
+  const treatmantReq = useAxios<TreatmentType>();
+  const phasesReq = useAxios<PhaseType[]>();
+
   const [modal, setModal] = useState(false);
   const [phaseSelected, setPhaseSelected] = useState<PhaseType | null>(null);
+  const [phases, setPhases] = useState<PhaseType[]>([]);
 
   function selectPhase(phase: PhaseType) {
     setPhaseSelected(
       phase === phaseSelected ? null : phase
     )
   }
+
+  useEffect(() => {
+    const { url, headers } = userRequest.GET_PATIENTS_BY_ID(patientId, token);
+    patientReq.get(url, { headers });
+  }, [patientId, token]);
+
+  useEffect(() => {
+    const { url, headers } = userRequest.GET_TREATMENTS_BY_ID(patientId, token);
+    treatmantReq.get(url, { headers });
+  }, [patientId, token]);
+
+  useEffect(() => {
+    try {
+      const { url, headers } = userRequest.GET_PHASES_BY_TREATMENTS_ID(String(treatmantReq.data?.id), token);
+      phasesReq.get(url, { headers });
+    }
+    catch (error) {
+      console.error(error);
+    }
+  }, [token, treatmantReq.data]);
+
+  useEffect(() => {
+    if (phasesReq.data) {
+      setPhases(phasesReq.data)
+    }
+  }, [token, phasesReq.data]);
+
+
   return (
     <Main>
-      <DefaultPatientPage patient={patient} >
+      <DefaultPatientPage patient={patientReq.data} >
         <Section>
-          <Title>Fase</Title>
+          <Title>Fases</Title>
           {phases.map((phase: PhaseType) =>
             <PhaseBlock key={phase.phaseNumber}>
               <PhaseTitle onClick={() => selectPhase(phase)}>
--- src/requests/UserRequest.ts
+++ src/requests/UserRequest.ts
@@ -1,4 +1,4 @@
-const BASE_URL = "https://app-vacina-production.up.railway.app";
+export const BASE_URL = "https://app-vacina-production.up.railway.app";
 
 type SignupRequestBody = {
   name: string;
@@ -7,85 +7,90 @@ type SignupRequestBody = {
 };
 
 export class UserRequest {
-  
-    USER_LOGIN = () => {
-        return {
-            url: `${BASE_URL}/login/doctors`
-        }
-    }
-
-    USER_SIGNUP = (body: SignupRequestBody) => {
-        return {
-            url: `${BASE_URL}/doctors`,
-            options: {
-                body,
-            },
-        };
+  USER_LOGIN = () => {
+    return {
+      url: `${BASE_URL}/login/doctors`,
     };
+  };
 
-    GET_DOCTOR_BY_ID = (id: any, token: string | null) => {
-        return {
-            url: `${BASE_URL}/doctors/${id}`,
-            headers: {
-                Authorization: token
-            }
-        }
-    }
+  USER_SIGNUP = (body: SignupRequestBody) => {
+    return {
+      url: `${BASE_URL}/doctors`,
+      options: {
+        body,
+      },
+    };
+  };
 
-    GET_PATIENTS = (token: string | null) => {
-        return {
-            url: `${BASE_URL}/doctors/patients`,
-            headers: {
-                Authorization: token
-            }
-        }
-    }
+  GET_DOCTOR_BY_ID = (id: any, token: string | null) => {
+    return {
+      url: `${BASE_URL}/doctors/${id}`,
+      headers: {
+        Authorization: token,
+      },
+    };
+  };
 
-    GET_PATIENTS_BY_ID = (id: string | undefined, token: string | null) => {
-        return {
-            url: `${BASE_URL}/patients/${id}`,
-            headers: {
-                Authorization: token,
-            }
-        }
-    }
+  GET_PATIENTS = (token: string | null) => {
+    return {
+      url: `${BASE_URL}/doctors/patients`,
+      headers: {
+        Authorization: token,
+      },
+    };
+  };
 
+  GET_PATIENTS_BY_ID = (id: string | undefined, token: string | null) => {
+    return {
+      url: `${BASE_URL}/patients/${id}`,
+      headers: {
+        Authorization: token,
+      },
+    };
+  };
 
-    GET_TREATMENTS_BY_ID = (id: string | undefined, token: string | null) => {
-        return {
-            url: `${BASE_URL}/treatments/patients/${id}`,
-            headers: {
-                Authorization: token
-            }
-        }
-    }
+  GET_TREATMENTS_BY_ID = (id: string | undefined, token: string | null) => {
+    return {
+      url: `${BASE_URL}/treatments/patients/${id}`,
+      headers: {
+        Authorization: token,
+      },
+    };
+  };
 
-    GET_PHASES_BY_TREATMENTS_ID = (id: string | undefined, token: string | null) => {
-        return {
-            url: `${BASE_URL}/phases/treatments/${id}`,
-            headers: {
-                Authorization: token
-            }
-        }
-    }
+  GET_PHASES_BY_TREATMENTS_ID = (
+    id: string | undefined,
+    token: string | null
+  ) => {
+    return {
+      url: `${BASE_URL}/phases/treatments/${id}`,
+      headers: {
+        Authorization: token,
+      },
+    };
+  };
 
-    GET_VACCINES_BY_PHASES_ID = (id: string | undefined, token: string | null) => {
-        return {
-            url: `${BASE_URL}/vaccines/phases/${id}`,
-            headers: {
-                Authorization: token
-            }
-        }
-    }
+  GET_VACCINES_BY_PHASES_ID = (
+    id: string | undefined,
+    token: string | null
+  ) => {
+    return {
+      url: `${BASE_URL}/vaccines/phases/${id}`,
+      headers: {
+        Authorization: token,
+      },
+    };
+  };
 
-    GET_TREATMENTS_PHASES_BY_ID = (id: string | undefined, token: string | null) => {
-        return {
-          url: `${BASE_URL}/phases/treatments/${id}`,
-          headers: {
-            Authorization: token,
-          },
-        };
-      };
-
-
+  GET_TREATMENTS_PHASES_BY_ID = (
+    id: string | undefined,
+    token: string | null
+  ) => {
+    return {
+      url: `${BASE_URL}/phases/treatments/${id}`,
+      headers: {
+        Authorization: token,
+      },
+    };
+  };
 }
