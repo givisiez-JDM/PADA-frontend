226cc9ecd4cdfe405eda4c7d44d0204cdb16ed7b
artphil <artphil.dev@gmail.com>
2024-03-07 17:38:34 -0300

    fix: corrigindo importes e estilos

--- src/pages/fasesDeTratamento/FasesDeTratamentoPage.styles.ts
+++ src/pages/fasesDeTratamento/FasesDeTratamentoPage.styles.ts
@@ -1,6 +1,4 @@
-import Switch from "@mui/material/Switch";
 import styled from "styled-components";
-
 
 export const HeaderContainer = styled.div`
   display: grid;
@@ -16,53 +14,48 @@ export const HeaderContainer = styled.div`
   background: #4c7abb;
   color: #ffffff;
   border-radius: 0 0 20px 20px;
-   
-    h1{
-  font-size: 35px;
-  font-weight: bold;
-  font-family: sans-serif;
-  color: ${(props) => props.theme['white']};
+
+  h1 {
+    font-size: 35px;
+    font-weight: bold;
+    font-family: sans-serif;
+    color: ${(props) => props.theme.colors.white};
   }
 `;
 
-
-
-
 export const SectionDoctor = styled.div`
-    display: flex;
-    margin-right: 6rem;
-    color: white;
-    font-size: larger;
-    align-items: center;
-    justify-content: end;
-    padding-right: 2rem;
-    
+  display: flex;
+  margin-right: 6rem;
+  color: white;
+  font-size: larger;
+  align-items: center;
+  justify-content: end;
+  padding-right: 2rem;
 
-    img {
-       margin-left: 1rem;
-       height: 1.3rem;
-       width: 0.6rem;
-       cursor: pointer;
-    }
-`
+  img {
+    margin-left: 1rem;
+    height: 1.3rem;
+    width: 0.6rem;
+    cursor: pointer;
+  }
+`;
 
+export const FaseContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: #334c76;
+  max-width: 25vw;
+  margin-top: 2vh;
+  margin-left: 5vw;
+  padding-bottom: 1rem;
 
-// export const FaseContentContainer = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   color: #334c76;
-//   max-width: 25vw;
-//   margin-top: 2vh;
-//   margin-left: 5vw;
-//   padding-bottom: 1rem;
-
-//  .faseOne{
-//   border-left:4px solid ${(props) => props.theme['variant-2']} ;
-//   border-radius: 2px;
-//   padding-left: 0.875rem;
-//   margin-bottom: 1.1rem;
-//  }
-// `;
+  .faseOne {
+    border-left: 4px solid ${(props) => props.theme.colors.variant2};
+    border-radius: 2px;
+    padding-left: 0.875rem;
+    margin-bottom: 1.1rem;
+  }
+`;
 
 export const ConteinerTreatment = styled.div`
   display: flex;
@@ -72,78 +65,66 @@ export const ConteinerTreatment = styled.div`
   margin-bottom: 1rem;
 `;
 
-// export const PeriodicidadeTratamento = styled.div`
+export const PeriodicidadeTratamento = styled.div``;
 
+export const DosagemMedicamento = styled.div``;
 
-// `;
+export const Title = styled.h3`
+  font-family: sans-serif;
+  font-weight: bold;
+  font-size: 1.125rem;
+  color: ${(props) => props.theme.colors.keyboard};
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+`;
 
-// export const DosagemMedicamento = styled.div`
+export const Inativa = styled.div`
+  display: flex;
+  gap: 1rem;
+  margin-bottom: 0.5rem;
+`;
 
-// `;
+export const ConteinerCheckBox = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  margin-top: 1rem;
+  gap: 0.563rem;
+`;
 
-// export const Title = styled.h3`
-// font-family: sans-serif;
-// font-weight: bold;
-// font-size: 1.125rem;
-// color: ${(props) => props.theme['keyboard']};
-// margin-top: 1rem;
-// margin-bottom: 1rem;
-
-// `;
-
-// export const Inativa = styled.div`
-//   display: flex;
-//   gap: 1rem;
-//   margin-bottom: 0.5rem;
-// `;
-
-// export const ConteinerCheckBox = styled.div`
- 
-//  display: grid;
-//  grid-template-columns: 1fr 1fr;
-//  margin-top: 1rem;
-//  gap: 0.563rem;
-
-`
 export const ContainerCheckbox = styled.div`
- 
- .checkbox {
-  display: none;
-}
-.checkbox + label:before {
-  content: '';
-  display: inline-block;
-  width: 1em;
-  height: 1em;
-  border-radius: 3px;
-  margin-right: 5px;
-  border: 2px solid black;
-  margin-bottom: -2px;
-  background: ${(props) => props.theme['Button-off']}
-}
-
-
-.custom-checkbox input:checked + label:before{
-  background-image: url('../../../src/assets/Vector.png');
-  background-position: center;
+  .checkbox {
+    display: none;
   }
-`
+  .checkbox + label:before {
+    content: "";
+    display: inline-block;
+    width: 1em;
+    height: 1em;
+    border-radius: 3px;
+    margin-right: 5px;
+    border: 2px solid black;
+    margin-bottom: -2px;
+    background: ${(props) => props.theme.colors.buttonOff};
+  }
+
+  .custom-checkbox input:checked + label:before {
+    background-image: url("../../../src/assets/Vector.png");
+    background-position: center;
+  }
+`;
 
 export const ButtonLocal = styled.div`
-margin-top: 40px;
-
-`
+  margin-top: 40px;
+`;
 
 export const TextInputTime = styled.input`
   background-color: #f5f5f5;
   border-radius: 15px;
   padding: 10px 10px;
   border: 1px solid #334c76;
-  color: #334C76;
-
-`
+  color: #334c76;
+`;
 
 export const SwitchContainer = styled.div`
-
-margin-top: 40px;
-`
+  margin-top: 40px;
+`;
--- src/pages/fasesDeTratamento/FasesDeTratamentoPage.tsx
+++ src/pages/fasesDeTratamento/FasesDeTratamentoPage.tsx
@@ -1,36 +1,33 @@
-import { ButtonLocal, ContainerCheckbox, ConteinerCheckBox, ConteinerTreatment, FaseContentContainer,  HeaderContainer,  SectionDoctor,  SwitchContainer,  TextInputTime,  Title } from "./styles";
-import Button from "../../components/Button/Button";
+import Button from "../../components/button/Button";
 import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import {  useState } from "react";
-import Stack from "@mui/material/Stack";
-import Typography from "@mui/material/Typography";
-import { styled } from "@mui/material/styles";
-import Switch from "@mui/material/Switch";
-import whiteArrow from "../../assets/white-arrow.svg";
-import { UserRequest } from "../../Requests/UserRequest";
-import { useNavigate } from "react-router-dom";
-import useAxios from "../../Hooks/useAxios";
-import { useData } from "../../Global/UserContext";
+import { useState } from "react";
+import { UserRequest } from "../../requests/UserRequest";
+import useAxios from "../../hooks/useAxios";
+import { useData } from "../../global/UserContext";
 import React from "react";
-import ModalDoctor from "../../components/ModalDoctor/ModalDoctor";
+import ModalDoctor from "../../components/modalDoctor/ModalDoctor";
+import { ButtonLocal, ContainerCheckbox, ConteinerCheckBox, ConteinerTreatment, FaseContentContainer, SwitchContainer, TextInputTime, Title } from "./FasesDeTratamentoPage.styles";
+import DefaultPatientPage from "../../components/defaultPatientPage/DefaultPatientPage";
+import { PatientType } from "../../types/PatientTypes";
 
 const faseOne = z.object({
-start:z.string().nonempty('Data inválida'),
-end:z.string().nonempty('Data inválida'),
-times:z.array(z.object({time: z.string()})),
-dosagens:z.array(z.object(
-  {dosagem: z.string()})),
+  start: z.string().nonempty('Data inválida'),
+  end: z.string().nonempty('Data inválida'),
+  times: z.array(z.object({ time: z.string() })),
+  dosagens: z.array(z.object(
+    { dosagem: z.string() })),
 
 })
 type RegisterFaseOne = z.infer<typeof faseOne>
 
 
+const patient: PatientType = { birthDate: '', email: '', id: '', name: 'Teste', photo: '', telephone: '' }
+
 export default function FasesDeTratamento() {
 
   const userRequest = new UserRequest();
-  const navigate = useNavigate();
   const patients = useAxios();
   const { userId, getProfile, data } = useData();
 
@@ -47,15 +44,6 @@ export default function FasesDeTratamento() {
     patients.get(url, { headers });
   }, []);
 
-  const getAllPatients = patients.data?.map((patient: any) => {
-    return (
-      <li onClick={() => navigate(`/perfil/paciente/${patient.id}`)} key={patient.id}>
-        {patient.name}
-      </li>
-    );
-  });
-
-
 
 
   const {
@@ -65,181 +53,183 @@ export default function FasesDeTratamento() {
   } = useForm<RegisterFaseOne>({
     resolver: zodResolver(faseOne),
     defaultValues: {
-       times:[{time:' cada 7 dias'},
-              {time:'cada 3 semanas'},
-              {time:'cada 2 semanas'},
-            {time:'A cada 4 semanas'},
-           ],
-    dosagens:[{dosagem:'1:10.000'},
-              {dosagem:'1:100'},
-              {dosagem:'1:1.000'},
-              {dosagem:'1:10'}, 
-            ] 
-  }
+      times: [{ time: ' cada 7 dias' },
+      { time: 'cada 3 semanas' },
+      { time: 'cada 2 semanas' },
+      { time: 'A cada 4 semanas' },
+      ],
+      dosagens: [{ dosagem: '1:10.000' },
+      { dosagem: '1:100' },
+      { dosagem: '1:1.000' },
+      { dosagem: '1:10' },
+      ]
+    }
   })
 
   const onSubmit = handleSubmit((data) => {
-    console.log("DATA SENT",data)
-   
+    console.log("DATA SENT", data)
+
   })
 
 
-const times: Array<string> = ['cada 7 dias','cada 3 semanas','cada 2 semanas','cada 4 semanas']
+  const times: Array<string> = ['cada 7 dias', 'cada 3 semanas', 'cada 2 semanas', 'cada 4 semanas']
 
-const dosagens: Array<string> = ['1:10.000','1:100',' 1:1.000','1:10']
-
-
+  const dosagens: Array<string> = ['1:10.000', '1:100', ' 1:1.000', '1:10']
 
 
-const AntSwitch = styled(Switch)(({ theme }) => ({
-  width: 28,
-  height: 16,
-  padding: 0,
-  display: 'flex',
-  '&:active': {
-    '& .MuiSwitch-thumb': {
-      width: 15,
-    },
-    '& .MuiSwitch-switchBase.Mui-checked': {
-      transform: 'translateX(9px)',
-    },
-  },
-  '& .MuiSwitch-switchBase': {
-    padding: 2,
-    '&.Mui-checked': {
-      transform: 'translateX(12px)',
-      color: '#fff',
-      '& + .MuiSwitch-track': {
-        opacity: 1,
-        backgroundColor: theme.palette.mode === 'dark' ? '#177ddc' : '#1890ff',
-      },
-    },
-  },
-  '& .MuiSwitch-thumb': {
-    boxShadow: '0 2px 4px 0 rgb(0 35 11 / 20%)',
-    width: 12,
-    height: 12,
-    borderRadius: 6,
-    transition: theme.transitions.create(['width'], {
-      duration: 200,
-    }),
-  },
-  '& .MuiSwitch-track': {
-    borderRadius: 16 / 2,
-    opacity: 1,
-    backgroundColor:
-      theme.palette.mode === 'dark' ? 'rgba(255,255,255,.35)' : 'rgba(0,0,0,.25)',
-    boxSizing: 'border-box',
-  },
-}));
 
 
-const [value, setValue] = useState(false)
+  // const AntSwitch = styled(Switch)(({ theme }) => ({
+  //   width: 28,
+  //   height: 16,
+  //   padding: 0,
+  //   display: 'flex',
+  //   '&:active': {
+  //     '& .MuiSwitch-thumb': {
+  //       width: 15,
+  //     },
+  //     '& .MuiSwitch-switchBase.Mui-checked': {
+  //       transform: 'translateX(9px)',
+  //     },
+  //   },
+  //   '& .MuiSwitch-switchBase': {
+  //     padding: 2,
+  //     '&.Mui-checked': {
+  //       transform: 'translateX(12px)',
+  //       color: '#fff',
+  //       '& + .MuiSwitch-track': {
+  //         opacity: 1,
+  //         backgroundColor: theme.palette.mode === 'dark' ? '#177ddc' : '#1890ff',
+  //       },
+  //     },
+  //   },
+  //   '& .MuiSwitch-thumb': {
+  //     boxShadow: '0 2px 4px 0 rgb(0 35 11 / 20%)',
+  //     width: 12,
+  //     height: 12,
+  //     borderRadius: 6,
+  //     transition: theme.transitions.create(['width'], {
+  //       duration: 200,
+  //     }),
+  //   },
+  //   '& .MuiSwitch-track': {
+  //     borderRadius: 16 / 2,
+  //     opacity: 1,
+  //     backgroundColor:
+  //       theme.palette.mode === 'dark' ? 'rgba(255,255,255,.35)' : 'rgba(0,0,0,.25)',
+  //     boxSizing: 'border-box',
+  //   },
+  // }));
 
-const handleChange = (event:any) => {
-  setValue(event.target.checked)
-  console.log(value)
-}
+
+  const [value, setValue] = useState(false)
+
+  const handleChange = (event: any) => {
+    setValue(event.target.checked)
+    console.log(value)
+  }
 
   return (
-    <>
-    <HeaderContainer>
-         <h1>Fase de Tratamento</h1>
-          <SectionDoctor onClick={() => setModal(!modal)}>
-            {data?.name}
-            <img src={whiteArrow} alt="" />
-          </SectionDoctor>
-    </HeaderContainer>
-        {/* <img src={doctor?.data?.photo?.data} alt="" /> */}
+    <DefaultPatientPage patient={patient}>
+      {/* <HeaderContainer>
+        <h1>Fase de Tratamento</h1>
+        <SectionDoctor onClick={() => setModal(!modal)}>
+          {data?.name}
+          <img src={whiteArrow} alt="" />
+        </SectionDoctor>
+      </HeaderContainer> */}
+      {/* <img src={doctor?.data?.photo?.data} alt="" /> */}
 
-      
+
       <form onSubmit={onSubmit}>
-      <FaseContentContainer>
-        <h2 className="faseOne">Fase 1</h2>
-        <ConteinerTreatment>
-          <Title>
-            Duração do Tratamento
-          </Title>
-           <p>Início</p>
-           <TextInputTime  type="date" style={{ width: "12em" }} 
-           {...register('start')}/>
-           {errors.start &&  <span>{errors.start.message}</span>}
-           <p>Fim</p>
-          <TextInputTime  type="date" style={{ width: "12em" }}
-          {...register('end')} />
-          {errors.end && <span>{errors.end.message}</span>}
-        </ConteinerTreatment>
+        <FaseContentContainer>
+          <h2 className="faseOne">Fase 1</h2>
+          <ConteinerTreatment>
+            <Title>
+              Duração do Tratamento
+            </Title>
+            <p>Início</p>
+            <TextInputTime type="date" style={{ width: "12em" }}
+              {...register('start')} />
+            {errors.start && <span>{errors.start.message}</span>}
+            <p>Fim</p>
+            <TextInputTime type="date" style={{ width: "12em" }}
+              {...register('end')} />
+            {errors.end && <span>{errors.end.message}</span>}
+          </ConteinerTreatment>
 
-        <div>
-          <Title>
-            Periodicidade do Tratamento
-          </Title>
-         
-          <ConteinerCheckBox>
-          {times.map(time => (
-            <ContainerCheckbox key={time}>
-            <div className="custom-checkbox" >
-              <input id={time} className="checkbox"
-                type="checkbox"
-                value={time}
-                {...register('times')}
-              />
-              <label htmlFor={time}>{time}</label>
-            </div>
-          </ ContainerCheckbox>
-            
-          ))}
-          </ConteinerCheckBox>
+          <div>
+            <Title>
+              Periodicidade do Tratamento
+            </Title>
 
-        </div>
-        <div>
-          
-          <Title>
-            Dosagem do Medicamento
-          </Title>
-          <ConteinerCheckBox>
-          {dosagens.map(dosagem => (
-            <ContainerCheckbox key={dosagem}>
-            <div className="custom-checkbox" >
-              <input id={dosagem} className="checkbox"
-                type="checkbox"
-                value={dosagem}
-                {...register('dosagens')}
-              />
-              <label htmlFor={dosagem}>{dosagem}</label>
-            </div>
-          </ ContainerCheckbox>
-            
-          ))}
-          </ConteinerCheckBox>
-        </div>
-        
-        <SwitchContainer>
-        <Stack direction="row" spacing={1} alignItems="center">
-        <Typography>Inativa</Typography>
-        <AntSwitch 
-        onChange={handleChange}
-        inputProps={{ 'aria-label': 'ant design' }} />
-        </Stack>
-       
-        </SwitchContainer>
-        
-       <ButtonLocal>
-          <Button color="primary" title="enviar conviter" 
-          type="submit"
-          style={{ width: "10rem", 
-           fontSize:'1.125rem',
-           fontFamily:'sans-serif' }}>
-           Enviar
-           </Button>
-       </ButtonLocal>
-       
+            <ConteinerCheckBox>
+              {times.map(time => (
+                <ContainerCheckbox key={time}>
+                  <div className="custom-checkbox" >
+                    <input id={time} className="checkbox"
+                      type="checkbox"
+                      value={time}
+                      {...register('times')}
+                    />
+                    <label htmlFor={time}>{time}</label>
+                  </div>
+                </ ContainerCheckbox>
 
-      </FaseContentContainer>
-    
-    </form>
-    {modal && <ModalDoctor />}
-    </>
+              ))}
+            </ConteinerCheckBox>
+
+          </div>
+          <div>
+
+            <Title>
+              Dosagem do Medicamento
+            </Title>
+            <ConteinerCheckBox>
+              {dosagens.map(dosagem => (
+                <ContainerCheckbox key={dosagem}>
+                  <div className="custom-checkbox" >
+                    <input id={dosagem} className="checkbox"
+                      type="checkbox"
+                      value={dosagem}
+                      {...register('dosagens')}
+                    />
+                    <label htmlFor={dosagem}>{dosagem}</label>
+                  </div>
+                </ ContainerCheckbox>
+
+              ))}
+            </ConteinerCheckBox>
+          </div>
+
+          <SwitchContainer>
+            {/* <Stack direction="row" spacing={1} alignItems="center">
+              <Typography>Inativa</Typography>
+              <AntSwitch
+                onChange={handleChange}
+                inputProps={{ 'aria-label': 'ant design' }} />
+            </Stack> */}
+
+          </SwitchContainer>
+
+          <ButtonLocal>
+            <Button color="primary" title="enviar conviter"
+              type="submit"
+              style={{
+                width: "10rem",
+                fontSize: '1.125rem',
+                fontFamily: 'sans-serif'
+              }}>
+              Enviar
+            </Button>
+          </ButtonLocal>
+
+
+        </FaseContentContainer>
+
+      </form>
+      {modal && <ModalDoctor />}
+    </DefaultPatientPage>
   );
 }
 
