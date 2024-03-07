5a9c6b4f9305afc37b5ef9ab435bc085fe0aed85
artphil <artphil.dev@gmail.com>
2024-03-07 16:55:14 -0300

    feat: restaurando componente

---
+++ src/components/button/Button.styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const ButtonLogin = styled.button`
+  background-color: #4c7abb;
+  color: white;
+  border: none;
+  border-radius: 15px;
+  width: 13rem;
+  height: 2rem;
+  font-weight: 500;
+`;
