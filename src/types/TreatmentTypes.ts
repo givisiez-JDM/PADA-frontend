2bca0c6f41cd061a76a27927c00c69a2c8a2800d
artphil <artphil.dev@gmail.com>
2024-03-19 17:32:37 -0300

    chore: ponto e virgula e quebra de linha

--- src/types/TreatmentTypes.ts
+++ src/types/TreatmentTypes.ts
@@ -1,31 +1,43 @@
-export type AllergieType = 'comida' | 'vespa' | 'ácaro' | 'gato' | 'bola' | 'inseto' | 'abelha' | 'formiga' | 'cachorro' | 'barata' | 'fungos' | 'candidíase'
-export type DosageType = '1:10' | '1:100' | '1:1.000'| '1:10.000'
-export type FrequencyType =  '7 dias' | '2 semanas' | '3 semanas' | '4 semanas'
+export type AllergyType =
+  | "comida"
+  | "vespa"
+  | "ácaro"
+  | "gato"
+  | "bola"
+  | "inseto"
+  | "abelha"
+  | "formiga"
+  | "cachorro"
+  | "barata"
+  | "fungos"
+  | "candidíase";
+export type DosageType = "1:10" | "1:100" | "1:1.000" | "1:10.000";
+export type FrequencyType = "7 dias" | "2 semanas" | "3 semanas" | "4 semanas";
 
 export type TreatmentType = {
-    id: string,
-    allergies: AllergieType[],
-    method: string,
-    active: boolean,
-    patientId?: number,
-}
+  id: string;
+  allergies: AllergyType[];
+  method: string;
+  active: boolean;
+  patientId?: number;
+};
 
 export type PhaseType = {
-    id: string,
-    phaseNumber: number,
-    dosage: DosageType,
-    frequency:  FrequencyType,
-    startTreatment: string,
-    endTreatment: string,
-    active: boolean,
-}
+  id: string;
+  phaseNumber: number;
+  dosage: DosageType;
+  frequency: FrequencyType;
+  startTreatment: string;
+  endTreatment: string;
+  active: boolean;
+};
 
 export type VaccineType = {
-    id: string,
-    title: string,
-    observation: string,
-    status: string,
-    scheduledDate: string,
-    applicationDate: string,
-    phaseId?: number,
-}
+  id: string;
+  title: string;
+  observation: string;
+  status: string;
+  scheduledDate: string;
+  applicationDate: string;
+  phaseId?: number;
+};
