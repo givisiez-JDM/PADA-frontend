a24d9fd0d9be99ddc5760ec0f8771952f8598baa
Arthur Phillip D. Silva <artphil.dev@gmail.com>
2024-03-25 13:52:49 -0300

    feat: [PADA 195] tela fases de tratamento (#40)
    * refactor: atualização do tipo Fase
    
    * feat: criando classe para request
    
    * fix: tipo resposta do request de tratamento
    
    * feat: atualizando tipo alergia
    
    * feat: atualizando tipo Tratamento
    
    * refactor: removendo elementos não utilizados
    
    * refactor: renomeando componente (padrão ingles)
    
    * fix: corrigindo tipo
    
    * fix: corigindo tipos
    
    * refactor: populando dados de forma dinâmica
    
    * feat: atualizando e utilizando o botão padrão
    
    * chore: removendo código não utilizado
    
    * chore: mudando rota padrão
    
    * feat: mostrando e ocultando as fases
    
    * feat: Criado um cartão com o progresso da fase
    
    * chore: prettier
    
    * fix: parse float
    
    * feat: imagem de seta
    
    * chore: corrigindo alt da imagem
    
    * feat: integração com backend
    
    * style: mudando posição do botão Alterar
    
    * feat: layout de paciente sem fase
    
    * feat: pedindo confirmação para finalizar a fase
    
    * doc: update CHANGELOG
    
    * refactor: movendo chamadas de servilço para o contexto
    
    * fix: corrigingo referencias
    
    * feat calculando o progresso da fase
    
    * fix: delay bug
    
    * feat: rota para alterar fase
    
    * doc: correção do changelog
    
    * refactor: apagando arquivo não utilizado
    
    * chore: ponto e virgula
    
    * chore ponto e virgula
    
    * fix: corrigindo mensagens de erro
    
    * refactor: extraindo calculo do HTML
    
    * chore: ponto e virgula e quebra de linha
    
    * chore: ponto e virgula
    
    * chore: ponto e virgula e quebra de linha
    
    * chore: ponto e virgula
    
    * chore: ponto e virgula
    
    * chore aspas duplas
    
    * chore: aspas duplas

---
+++ src/pages/treatmentPhases/TreatmentPhases.tsx
@@ -0,0 +1,149 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import useAxios from "../../hooks/useAxios";
+import { useData } from "../../global/UserContext";
+import { UserRequest } from "../../requests/UserRequest";
+import { PhaseType, VaccineType } from "../../types/TreatmentTypes";
+import IconArrowUp from '../../assets/arrow-up.svg';
+import IconArrowDown from '../../assets/arrow-down.svg';
+import DefaultPatientPage from "../../components/defaultPatientPage/DefaultPatientPage";
+import Button from "../../components/button/Button";
+import ModalTreatmentPhase from "../../components/modalTreatmentPhase/ModalTreatmentPhase";
+import Phase from "./phase/Phase";
+import { BoxButton, Main, PhaseBlock, PhaseTitle, Section, Title } from "./TreatmentPhases.styles";
+
+const TreatmentPhases = () => {
+  const { id: idPatient } = useParams();
+  const {
+    getToken,
+    patient,
+    patientId,
+    setPatientId,
+    getPatient,
+    getTreatment,
+    treatmentId,
+    phaseList,
+    getPhaseList,
+    setPhaseId,
+    phaseId,
+    getVaccineList,
+    vaccineList,
+  } = useData();
+
+  const userRequest = new UserRequest();
+  const phaseReq = useAxios<PhaseType>();
+
+  const [modal, setModal] = useState(false);
+  const [phaseSelected, setPhaseSelected] = useState<PhaseType | null>(null);
+  const [phaseProgress, setPhaseProgress] = useState<number>(0);
+
+  const selectPhase = (phase: PhaseType) => {
+    const newPhase = phase === phaseSelected ? null : phase;
+    setPhaseSelected(newPhase);
+    setPhaseId(newPhase ? newPhase.id : '');
+  };
+
+  const hasPhases = () => phaseList.length > 0;
+
+  const finishPhase = () => {
+    if (!phaseSelected) return;
+
+    const token = getToken();
+    if (confirm('Tem certeza que deseja finalizar essa fase?')) {
+      const { url, headers } = userRequest.PUT_PHASE_STATUS_BY_ID(phaseSelected.id, token);
+
+      const body = {
+        phaseNumber: phaseSelected,
+        active: false,
+      };
+
+      phaseReq.put(url, body, { headers });
+    }
+  };
+
+  useEffect(() => {
+    if (idPatient && idPatient !== patientId)
+      setPatientId(idPatient);
+  }, [idPatient]);
+
+  useEffect(() => {
+    if (patientId) {
+      getPatient();
+      getTreatment();
+    }
+  }, [patientId]);
+
+  useEffect(() => {
+    getPhaseList();
+  }, [treatmentId, phaseReq.data]);
+
+  useEffect(() => {
+    getVaccineList();
+  }, [phaseId]);
+
+  useEffect(() => {
+    const total = vaccineList.length;
+    const applied = vaccineList.reduce((total: number, vaccine: VaccineType) => {
+      if (vaccine.status !== 'agendado') {
+        return total + 1;
+      }
+      return total;
+    }, 0);
+    setPhaseProgress(applied / total);
+  }, [vaccineList]);
+
+  const getArrow = (phaseNumber: number) => {
+    if (phaseSelected?.phaseNumber === phaseNumber) {
+      return IconArrowUp;
+    }
+    return IconArrowDown;
+  };
+
+  const showPhase = (phase: PhaseType) => {
+    if (phaseSelected?.phaseNumber === phase.phaseNumber) {
+      return (
+        <Phase phase={phase} progress={phaseProgress} />
+      );
+    }
+  };
+
+  const getPhases = () => {
+    return phaseList
+      .sort(((a, b) => a.phaseNumber - b.phaseNumber))
+      .map((phase: PhaseType) =>
+        <PhaseBlock key={phase.phaseNumber}>
+          <PhaseTitle onClick={() => selectPhase(phase)}>
+            Fase {phase.phaseNumber}
+            <img src={getArrow(phase.phaseNumber)} alt="Mostrar conteúdo" />
+          </PhaseTitle>
+          {showPhase(phase)}
+        </PhaseBlock>
+      );
+  };
+
+  return (
+    <Main>
+      <DefaultPatientPage patient={patient} >
+        <Section>
+          <Title>Fases</Title>
+          {getPhases()}
+          <BoxButton className={hasPhases() ? '' : 'centered'}>
+            {
+              hasPhases() &&
+              <Button
+                disabled={!phaseSelected?.active}
+                onClick={finishPhase}
+              >
+                Finalizar Fase
+              </Button>
+            }
+            <Button onClick={() => setModal(!modal)}>Adicionar Fase</Button>
+          </BoxButton>
+        </Section>
+        {modal && <ModalTreatmentPhase setModal={setModal} />}
+      </DefaultPatientPage>
+    </Main>
+  );
+};
+
+export default TreatmentPhases;
