cdf757330b6bac8a3e283049f40485428f513743
yasminconc <yasminsilvaconc@gmail.com>
2024-03-27 16:45:05 -0300

    Feat: Layout Tela Cadastro Paciente

--- src/pages/cadastroPaciente/CadastroPacientePage.styles.ts
+++ src/pages/cadastroPaciente/CadastroPacientePage.styles.ts
@@ -1,81 +1,135 @@
+import styled from "styled-components";
 
-import styled from "styled-components"
-
-export const Main = styled.div `
-    display: flex;
-    height: 100vh;
-    flex-direction: column;
-    background-color: blue;
+export const Main = styled.div`
+  display: flex;
+  flex-direction: column;
 `
 
 export const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    background-color: green;
-    gap: 1rem;
-    
+  display: flex;
+  flex-direction: column;
+  padding: 0 3rem;
+  gap: 1rem 0;
 `
 
-export const InputContent = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    background-color: violet;
-    gap: 1rem;
-    margin-top: 2rem;
-    margin-left: 2rem;
-    width: 35rem;
+export const FirstBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
 `
 
 export const BoxInput = styled.div`
-    display: flex;
-    flex-direction: column;
-    background-color: red;
-    width: 30rem;
-    
-    
+  display: flex;
+  flex-direction: column;
+  gap: 0.4rem 0;
+  font-weight: 500;
 `
 
 export const SecondBox = styled.div`
-    display: flex;
-    flex-direction: column;
-    background-color: yellow;
-    width: 35rem;
-    justify-content: center;
-    margin-left: 2rem;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem 0;
+  padding-top: 1.7rem;
 `
 
 export const TreatmentDuration = styled.div`
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem 0;
+  font-weight: 500;
 `
 
 export const Medication = styled.div`
-
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem 0;
+  font-weight: 700;
 `
 
 export const TreatmentMethod = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem 0;
 
+  div {
+    display: flex;
+    align-items: center;
+    gap: 0 1rem;
+
+    div {
+      gap: 0;
+    }
+  }
 `
 
-export const ThirdBox = styled.div`
-    display: flex;
-    flex-direction: column;
-    background-color: lightseagreen;
-    width: 35rem;
-    justify-content: center;
-    margin-left: 2rem;
-    align-items: center;
+export const MainContent = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0 10rem;
+`
+
+export const AllergiesContainer = styled.div`
+  display: flex;
+  gap: 0.5rem 0;
+  align-items: flex-end;
+  justify-content: space-between;
+`
+
+export const AllergiesBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem 0;
 `
 
 export const Allergies = styled.div`
-
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem 3rem;
+  flex-wrap: wrap;
+  height: 10rem;
+  width: auto;
 `
+
+export const LeftBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  img {
+    height: 3.5rem;
+    width: 4rem;
+  }
+
+  h1 {
+    margin-left: 1rem;
+    font-size: 1.3rem;
+  }
+`
+
+export const RigthBox = styled.div `
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 13rem;
+  cursor: pointer;
+  margin-right: 3rem;
+`
+
 export const BoxButton = styled.div`
-    display: flex;
-    justify-content: center;
+  button {
+    background-color: ${(props) => props.theme.colors.variant2};
+    outline: none;
+    border: none;
+    color: ${(props) => props.theme.colors.white};
+    padding: 0.5rem 3rem;
+    border-radius: 30px;
+    font-size: 1rem;
+    font-weight: 500;
+    margin-right: 5rem;
+
+    &:hover {
+      background-color: ${(props) => props.theme.colors.variant3};
+      transition: 0.3s;
+    }
+  }
 `
-
-
-
--- src/pages/cadastroPaciente/CadastroPacientePage.tsx
+++ src/pages/cadastroPaciente/CadastroPacientePage.tsx
@@ -1,144 +1,157 @@
-
-import Input from "../../components/input/Input"
-import MenuHeader from "../../components/menuHeader/MenuHeader"
-import { Allergies, BoxButton, BoxInput, Container, InputContent, Main, Medication, SecondBox, ThirdBox, TreatmentDuration, TreatmentMethod } from "./CadastroPacientePage.styles"
-
+import Input from "../../components/input/Input";
+import MenuHeader from "../../components/menuHeader/MenuHeader";
+import profileUser from "../../assets/profileUser.svg";
+import doctorPhoto from "../../assets/doctorPhoto.svg";
+import iconArrow from "../../assets/white-arrow.svg";
+import {
+  Allergies,
+  BoxButton,
+  BoxInput,
+  Container,
+  FirstBox,
+  Main,
+  MainContent,
+  Medication,
+  SecondBox,
+  AllergiesContainer,
+  TreatmentDuration,
+  TreatmentMethod,
+  AllergiesBox,
+  LeftBox,
+  RigthBox,
+} from "./CadastroPacientePage.styles";
+import ModalDoctor from "../../components/modalDoctor/ModalDoctor";
+import { useState } from "react";
 
 const CadastroPacientePage = () => {
+  const [modal, setModal] = useState(false);
   return (
     <Main>
-        <MenuHeader/>
-        <Container>  
-            <InputContent>
-                <BoxInput>
-                    <label htmlFor="">Nome</label>
-                    <Input 
-                        type="text"
-                        name="name"
-                    />
-                </BoxInput>
-                <BoxInput>
-                    <label htmlFor="">Telefone</label>
-                    <Input 
-                        type="text" 
-                        name="phone"
-                    />
-                </BoxInput>
-                <BoxInput>
-                    <label htmlFor="">E-mail</label>
-                    <Input 
-                        type="text" 
-                        name="email"
-                    />
-                </BoxInput>
-                <BoxInput>
-                    <label htmlFor="">Data de nascimento</label>
-                    <Input 
-                        type="text" 
-                        name="born"
-                    />
-                </BoxInput>
-            </InputContent>
+      <MenuHeader>
+        <LeftBox>
+          <img src={profileUser} alt="profile user" />
+          <h1>Novo Paciente</h1>
+        </LeftBox>
+        <RigthBox onClick={() => setModal(!modal)}>
+          <img src={doctorPhoto} alt="doctor photo" />
+          <h1>Dr. Antonio</h1>
+          <img src={iconArrow} alt="" />
+        </RigthBox>
+      </MenuHeader>
+      <Container>
+        <MainContent>
+          <FirstBox>
+            <BoxInput>
+              <label htmlFor="">Nome</label>
+              <Input type="text" name="name" />
+            </BoxInput>
+            <BoxInput>
+              <label htmlFor="">Telefone</label>
+              <Input type="text" name="phone" />
+            </BoxInput>
+            <BoxInput>
+              <label htmlFor="">E-mail</label>
+              <Input type="text" name="email" />
+            </BoxInput>
+            <BoxInput>
+              <label htmlFor="">Data de nascimento</label>
+              <Input type="text" name="born" />
+            </BoxInput>
+          </FirstBox>
 
-            <SecondBox>
-                <h1>Duração do Tratamento</h1>
-                <TreatmentDuration>
-                    <label htmlFor="">Inicío</label>
-                    <Input 
-                        type="text" 
-                        name="start"
-                    />
+          <SecondBox>
+            <h1>Duração do Tratamento</h1>
+            <TreatmentDuration>
+              <label htmlFor="">Inicío</label>
+              <Input type="text" name="start" />
 
-                    <label htmlFor="">Fim</label>
-                    <Input 
-                        type="text" 
-                        name="end"
-                    />
-                </TreatmentDuration>
+              <label htmlFor="">Fim</label>
+              <Input type="text" name="end" />
+            </TreatmentDuration>
 
-                <Medication>
-                    <h1>Medicação</h1>
-                    <Input
-                        type="text"
-                        name="medication"
-                    />
-                </Medication>
+            <Medication>
+              <label htmlFor="">Medicação</label>
+              <Input type="text" name="medication" />
+            </Medication>
 
-                <TreatmentMethod>
-                    <h1>Método de Tratamento</h1>
+            <TreatmentMethod>
+              <h1>Método de Tratamento</h1>
 
-                    <label htmlFor="">Sublingual</label>
-                    <input type="checkbox" name="" id="" />
+              <div>
+                <div>
+                  <label htmlFor="">Sublingual</label>
+                  <input type="checkbox" name="" id="" />
+                </div>
 
-                    <label htmlFor="">Vacinas</label>
-                    <input type="checkbox" name="" id="" />
-                </TreatmentMethod>
+                <div>
+                  <label htmlFor="">Vacinas</label>
+                  <input type="checkbox" name="" id="" />
+                </div>
+              </div>
+            </TreatmentMethod>
+          </SecondBox>
+        </MainContent>
 
-            </SecondBox>
+        <AllergiesContainer>
+          <AllergiesBox>
+            <h1>Alergias</h1>
+            <Allergies>
+              <div>
+                <input type="checkbox" name="" id="" />
+                <label htmlFor="">Alimentar</label>
+              </div>
+              <div>
+                <input type="checkbox" name="" id="" />
+                <label htmlFor="">Vespa</label>
+              </div>
+              <div>
+                <input type="checkbox" name="" id="" />
+                <label htmlFor="">Ácaro</label>
+              </div>
+              <div>
+                <input type="checkbox" name="" id="" />
+                <label htmlFor="">Gato</label>
+              </div>
+              <div>
+                <input type="checkbox" name="" id="" />
+                <label htmlFor="">Inseto</label>
+              </div>
+              <div>
+                <input type="checkbox" name="" id="" />
+                <label htmlFor="">Abelha</label>
+              </div>
+              <div>
+                <input type="checkbox" name="" id="" />
+                <label htmlFor="">Formiga</label>
+              </div>
+              <div>
+                <input type="checkbox" name="" id="" />
+                <label htmlFor="">Cachorro</label>
+              </div>
+              <div>
+                <input type="checkbox" name="" id="" />
+                <label htmlFor="">Barata</label>
+              </div>
+              <div>
+                <input type="checkbox" name="" id="" />
+                <label htmlFor="">Fungo</label>
+              </div>
+            </Allergies>
+            <div>
+              <input type="checkbox" name="" id="" />
+              <label htmlFor="">Outros:</label>
+            </div>
 
-            <ThirdBox>
-                <h1>Alergias</h1>
-                <Allergies>
-                    <div>
-                        <label htmlFor="">Alimentar</label>
-                        <input type="checkbox" name="" id="" />
-                    </div>
-                    <div>
-                        <label htmlFor="">Vespa</label>
-                        <input type="checkbox" name="" id="" />
-                    </div>
-                    <div>
-                        <label htmlFor="">Ácaro</label>
-                        <input type="checkbox" name="" id="" />
-                    </div>
-                    <div>
-                        <label htmlFor="">Gato</label>
-                        <input type="checkbox" name="" id="" />
-                    </div>
-                    <div>
-                        <label htmlFor="">Inseto</label>
-                        <input type="checkbox" name="" id="" />
-                    </div>
-                    <div>
-                        <label htmlFor="">Outros:</label>
-                        <input type="checkbox" name="" id="" />
-                    </div>
-                    <div>
-                        <label htmlFor="">Abelha</label>
-                        <input type="checkbox" name="" id="" />
-                    </div>
-                    <div>
-                        <label htmlFor="">Formiga</label>
-                        <input type="checkbox" name="" id="" />
-                    </div>
-                    <div>
-                        <label htmlFor="">Cachorro</label>
-                        <input type="checkbox" name="" id="" />
-                    </div>
-                    <div>
-                        <label htmlFor="">Barata</label>
-                        <input type="checkbox" name="" id="" />
-                    </div>
-                    <div>
-                        <label htmlFor="">Fungo</label>
-                        <input type="checkbox" name="" id="" />
-                    </div>
-                    
-                </Allergies>
-                <Input 
-                    type="text" 
-                    name="others"
-                />
-            </ThirdBox>
-
-            <BoxButton>
-                <button>Salvar</button>
-            </BoxButton>
-
-
-        </Container>
+            <Input type="text" name="others" />
+          </AllergiesBox>
+          <BoxButton>
+            <button>Salvar</button>
+          </BoxButton>
+        </AllergiesContainer>
+      </Container>
+      {modal && <ModalDoctor />}
     </Main>
-  )
-}
+  );
+};
 
-export default CadastroPacientePage
+export default CadastroPacientePage;
