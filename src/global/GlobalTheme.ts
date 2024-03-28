9986fe1262584bfcc842da0e7fd1fed968452054
artphil <artphil.dev@gmail.com>
2024-03-27 21:56:47 -0300

    fix: fix theme lint

--- src/global/GlobalTheme.ts
+++ src/global/GlobalTheme.ts
@@ -1,27 +1,28 @@
-import 'styled-components';
+import "styled-components";
 
 export const Theme = {
-    colors: {
-        variant1: '#76A7ED',
-        variant2: '#4C7ABB',
-        variant3: '#334C76',
-        variant4: '#E9EAEC',
-        separation: '#F5F5F5',
-        white: '#FFFFFF',
-        keyboard: '#273047',
-        background: '#E3E3E3',
-        buttonOff: '#B4B4B4',
-        vaccine: {
-            applied: '#5CED38',
-            notApplied: '#E85656',
-            late: '#FACB71',
-            schedule: '#B4B4B4',
-        }
+  colors: {
+    variant1: "#76A7ED",
+    variant2: "#4C7ABB",
+    variant3: "#334C76",
+    variant4: "#E9EAEC",
+    separation: "#F5F5F5",
+    white: "#FFFFFF",
+    keyboard: "#273047",
+    background: "#E3E3E3",
+    buttonOff: "#B4B4B4",
+    vaccine: {
+      applied: "#5CED38",
+      notApplied: "#E85656",
+      late: "#FACB71",
+      schedule: "#B4B4B4",
     },
-}
+  },
+};
 
-export type ITheme = typeof Theme
+export type ITheme = typeof Theme;
 
-declare module 'styled-components' {
-    export interface DefaultTheme extends ITheme { }
+declare module "styled-components" {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  export interface DefaultTheme extends ITheme {}
 }
