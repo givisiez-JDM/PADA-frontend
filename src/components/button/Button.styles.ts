39006b10fc706beedda4f43c4fee02df8270fb40
artphil <artphil.dev@gmail.com>
2024-04-10 19:55:22 -0300

    style: fix disabled color

--- src/components/button/Button.styles.ts
+++ src/components/button/Button.styles.ts
@@ -8,20 +8,6 @@ export const ButtonBase = styled.button`
   font-size: 1rem;
   font-weight: 500;
 
-  &:hover {
-    transition: 0.3s ease-in-out;
-    color: ${(props) => props.theme.colors.variant4};
-    scale: 1.05;
-  }
-
-  &:disabled {
-    background-color: ${(props) => props.theme.colors.buttonOff};
-    :hover {
-      transition: none;
-      scale: none;
-    }
-  }
-
   &.small {
     padding: 0.12rem 1.12rem;
     font-size: 0.8rem;
@@ -38,5 +24,19 @@ export const ButtonBase = styled.button`
 
   &.secondary {
     background-color: ${(props) => props.theme.colors.buttonOff};
+  }
+
+  &:hover {
+    transition: 0.3s ease-in-out;
+    color: ${(props) => props.theme.colors.variant4};
+    scale: 1.05;
+  }
+
+  &:disabled {
+    background-color: ${(props) => props.theme.colors.buttonOff};
+    :hover {
+      transition: none;
+      scale: none;
+    }
   }
 `;
