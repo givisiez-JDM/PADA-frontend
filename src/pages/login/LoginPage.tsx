e1c94da13595b37f07f8f4e4356a290b05d3ca96
artphil <artphil.dev@gmail.com>
2024-04-29 14:07:22 -0300

    refactor: rollback file

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -8,10 +8,8 @@ import eyesOpen from '../../assets/eyes-open.svg';
 import eyesClosed from '../../assets/eyes-closed.svg';
 import { useLogin } from '../../hooks/useForm';
 import { useNavigate } from 'react-router-dom';
-import {
-  BottomWave, Box, Checkbox, ForgotPassword, Image, Eyes,
-  IncorrectUser, InputBox, Main, PasswordInputBox, TopWave,
-} from './LoginPage.styles';
+import { BottomWave, Box, Checkbox, ForgotPassword, Image, Eyes,
+  IncorrectUser, InputBox, Main, PasswordInputBox, TopWave } from './LoginPage.styles';
 import { useData } from '../../global/UserContext';
 
 const Login = () => {
